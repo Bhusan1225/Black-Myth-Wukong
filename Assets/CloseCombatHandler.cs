43a1c67ae893a99d663e1a4053a86d20e305433b
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-04-12 10:28:55 +0530

    just change the name of the combat Manager

--- Assets/CloseCombatHandler.cs
+++ Assets/CloseCombatHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatManager : MonoBehaviour
+public class CloseCombatHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
