07d7c52651065d7b2a3ee118b3f098327306ce76
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-04-14 11:03:41 +0530

    done with the detecting of the player
    also added the enemy attack and death animation

--- Assets/ElderGoblinAI.cs
+++ Assets/ElderGoblinAI.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,13 +6,43 @@ using UnityEngine;
 
 public class ElderGoblinAI : MonoBehaviour
 {
-    public float maxHealth = 200;
-    public float currentHealth;
 
+    [SerializeField] float maxHealth = 200;
+    [SerializeField] float currentHealth;
+
+    [SerializeField]float speed = 4f;
+
+    [Header("Player detect")]
+    [SerializeField] GameObject player;
+    [SerializeField] LayerMask playerlayer;
+    [SerializeField] float visionRadious;
+    [SerializeField] bool isPlayerinVisionRadious;
 
     private void Start()
     {
         currentHealth = maxHealth;
+    }
+
+
+
+    private void Update()
+    {
+       isPlayerinVisionRadious = Physics.CheckSphere(transform.position, visionRadious,playerlayer);
+
+        if (isPlayerinVisionRadious) 
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            //idle animation
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        transform.position += transform.forward * speed *Time.deltaTime ;
+        transform.LookAt(player.transform);
     }
 
     public void TakeDamage(float amout)
